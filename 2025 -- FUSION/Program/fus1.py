5d2daa6bc12b3268dd6f541d2ac8f615700630a3
Pollapat Rattanapol <98020267+RETOUTHz@users.noreply.github.com>
2025-02-20 17:29:50 +0700

    ;

--- 2025 -- FUSION/Program/fus1.py
+++ 2025 -- FUSION/Program/fus1.py
@@ -20,11 +20,8 @@ en = {
     "LF": encoder_motor_class("M1", "INDEX1"), #Left_Front wheel
     "LB": encoder_motor_class("M2", "INDEX1"), #Left_Back wheel
     "RF": encoder_motor_class("M5", "INDEX1"), #Right_Front wheel 
-    "RB": encoder_motor_class("M6", "INDEX1")  #Right_Back wheel
-}
-
-sv = {
-    "s" : smartservo_class(""),"INDEX1")
+    "RB": encoder_motor_class("M6", "INDEX1"),  #Right_Back wheel
+    "FEED": encoder_motor_class("M4", "INDEX1")
 }
 """
 SYSTEM
@@ -42,58 +39,72 @@ def gripper(a:int):
     time.sleep(0.1)
     power_expand_board.set_power("DC8",10)
 
-def red_servo():
-    if sv["s6"].get_value("current") > 1250:
-        sv["s6"].set_power(0)
+def feed(a:int):
+    en["FEED"].set_power(-a)
+
+def stop_all():
+    power_expand_board.set_power("DC")
 
 class movement:
     def control_movement_font():            
-        rf = (gamepad.get_joystick("Lx") +gamepad.get_joystick("Rx")) * 0.75
-        lb = (gamepad.get_joystick("Lx") -gamepad.get_joystick("Rx")) * 0.75
-        lf = (gamepad.get_joystick("Ly") -gamepad.get_joystick("Rx")) * 0.75
-        rb = (gamepad.get_joystick("Ly") +gamepad.get_joystick("Rx")) * 0.75
+        rf = (gamepad.get_joystick("Lx") - -gamepad.get_joystick("Rx")) * 0.75
+        lb = (gamepad.get_joystick("Lx") + -gamepad.get_joystick("Rx")) * 0.75
+        lf = (gamepad.get_joystick("Ly") + -gamepad.get_joystick("Rx")) * 0.75
+        rb = (gamepad.get_joystick("Ly") - -gamepad.get_joystick("Rx")) * 0.75
         en["RF"].set_power(-rf)
         en["RB"].set_power(-rb)
         en["LB"].set_power(lb)
         en["LF"].set_power(lf)
     
     def control_movement_right():
-        rf = (gamepad.get_joystick("Lx") - gamepad.get_joystick("Rx")) * 0.75
-        lb = (gamepad.get_joystick("Lx") + gamepad.get_joystick("Rx")) * 0.75
-        lf = (gamepad.get_joystick("Ly") + gamepad.get_joystick("Rx")) * 0.75
-        rb = (gamepad.get_joystick("Ly") - gamepad.get_joystick("Rx")) * 0.75
+        rf = (gamepad.get_joystick("Ly") - gamepad.get_joystick("Rx")) * 0.75
+        lb = (gamepad.get_joystick("Ly") + gamepad.get_joystick("Rx")) * 0.75
+        lf = (-gamepad.get_joystick("Lx") + gamepad.get_joystick("Rx")) * 0.75
+        rb = (-gamepad.get_joystick("Lx") - gamepad.get_joystick("Rx")) * 0.75
         en["RF"].set_power(rf)
         en["RB"].set_power(rb)
-        en["LB"].set_power(lb)
-        en["LF"].set_power(lf)
+        en["LB"].set_power(-lb)
+        en["LF"].set_power(-lf)
 
 """
 CONTROLLER
 """
 class controller():
+    mode = "1"
     def mode1():
         movement.control_movement_font()
         if gamepad.is_key_pressed("Up"):
             lift(-100)
+
         elif gamepad.is_key_pressed("Down"):
             lift(100)
+
         elif gamepad.is_key_pressed("Right"):
             gripper(50)
+
         elif gamepad.is_key_pressed("Left"):
             gripper(-50)
-        elif gamepad.is_key_pressed(""):
-            blush(50)
-        elif gamepad.is_key_pressed(""):
-            blush(0)
-        elif gamepad.is_key_pressed(""):
-            sv["s6"].move_to(-55,50)
-        elif gamepad.is_key_pressed(""):
-            sv["s6"].move_to(-110,40)
+
     def mode2():
         movement.control_movement_right()
+        if gamepad.is_key_pressed("N1"):
+            feed(100)
+
+        elif gamepad.is_key_pressed("L1"):
+            feed(0)
+
+    def change_mode():
+        if gamepad.is_key_pressed("+"):
+            controller.mode = "1"
+        elif gamepad.is_key_pressed("≡"):
+            controller.mode = "2"
 
 """
 MAIN
 """
 while True:
-    controller.mode1()
+    controller.change_mode()
+    if controller.mode == "1":
+        controller.mode1()
+    else:
+        controller.mode2()

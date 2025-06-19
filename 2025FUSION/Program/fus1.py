b41eabc984b38c4df96f99aed1b927fc14f14853
Pollapat Rattanapol <98020267+RETOUTHz@users.noreply.github.com>
2025-06-19 09:02:55 +0700

    s

--- 2025FUSION/Program/fus1.py
+++ 2025FUSION/Program/fus1.py
@@ -30,6 +30,14 @@ sv = {
 
 debug = led_matrix_class("PORT5","INDEX1")
 """
+AUTO SECLET
+จะใช้autoตัวไหน #comment ออก
+"""
+def select():
+    Auto.Left_block_auto()
+    #Auto.Right_block_auto()
+    #Auto.Emergency()
+"""
 SYSTEM
 """
 
@@ -94,8 +102,8 @@ class movement:
     def control_movement_font():            
         rf = ((gamepad.get_joystick("Lx") + gamepad.get_joystick("Rx")) * 0.7) #+ math.fabs((gamepad.get_joystick("Ly") * 0.1))
         lb = (((gamepad.get_joystick("Lx") * 0.8 ) - gamepad.get_joystick("Rx")) * 0.7) #- math.fabs((gamepad.get_joystick("Ly") * 0.1))
-        lf = (gamepad.get_joystick("Ly") + -gamepad.get_joystick("Rx")) * 0.7 #- math.fabs((gamepad.get_joystick("Lx") * 0.05))
-        rb = (gamepad.get_joystick("Ly") - -gamepad.get_joystick("Rx")) * 0.7 #+ math.fabs((gamepad.get_joystick("Lx") * 0.05))
+        lf = (gamepad.get_joystick("Ly") + -gamepad.get_joystick("Rx")) * 0.8 #- math.fabs((gamepad.get_joystick("Lx") * 0.05))
+        rb = (gamepad.get_joystick("Ly") - -gamepad.get_joystick("Rx")) * 0.8 #+ math.fabs((gamepad.get_joystick("Lx") * 0.05))
         en["RF"].set_power(-rf)
         en["RB"].set_power(-rb)
         en["LB"].set_power(lb)
@@ -198,6 +206,9 @@ class controller():
             controller.mode = "1"
         elif gamepad.is_key_pressed("≡"):
             controller.mode = "2"
+"""
+AUTO
+"""
 
 """
 MAIN

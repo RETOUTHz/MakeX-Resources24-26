4aa4db2ffa5db8dbee66f1778943c84c0612b3d8
Pollapat Rattanapol <98020267+RETOUTHz@users.noreply.github.com>
2025-06-20 07:46:22 +0700

    asd

--- 2025FUSION/Program/fus1.py
+++ 2025FUSION/Program/fus1.py
@@ -131,10 +131,10 @@ CONTROLLER
 """
 class movement:
     def control_movement_font():            
-        rf = ((gamepad.get_joystick("Lx") + gamepad.get_joystick("Rx")) * 0.7) #+ math.fabs((gamepad.get_joystick("Ly") * 0.1))
-        lb = (((gamepad.get_joystick("Lx") * 0.8 ) - gamepad.get_joystick("Rx")) * 0.7) #- math.fabs((gamepad.get_joystick("Ly") * 0.1))
-        lf = (gamepad.get_joystick("Ly") + -gamepad.get_joystick("Rx")) * 0.8 #- math.fabs((gamepad.get_joystick("Lx") * 0.05))
-        rb = (gamepad.get_joystick("Ly") - -gamepad.get_joystick("Rx")) * 0.8 #+ math.fabs((gamepad.get_joystick("Lx") * 0.05))
+        rf = ((gamepad.get_joystick("Lx") + gamepad.get_joystick("Rx")) * 0.8) #+ math.fabs((gamepad.get_joystick("Ly") * 0.1))
+        lb = (((gamepad.get_joystick("Lx") * 0.8 ) - gamepad.get_joystick("Rx")) * 0.8) #- math.fabs((gamepad.get_joystick("Ly") * 0.1))
+        lf = (gamepad.get_joystick("Ly") + -gamepad.get_joystick("Rx")) * 0.7 #- math.fabs((gamepad.get_joystick("Lx") * 0.05))
+        rb = (gamepad.get_joystick("Ly") - -gamepad.get_joystick("Rx")) * 0.7 #+ math.fabs((gamepad.get_joystick("Lx") * 0.05))
         en["RF"].set_power(-rf)
         en["RB"].set_power(-rb)
         en["LB"].set_power(lb)

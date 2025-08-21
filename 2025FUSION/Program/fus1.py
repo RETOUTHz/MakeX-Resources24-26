f81efd73db10a26a419496fc4746c1e10e832072
Pollapat Rattanapol <98020267+RETOUTHz@users.noreply.github.com>
2025-08-21 09:54:52 +0700

    th

--- 2025FUSION/Program/fus1.py
+++ 2025FUSION/Program/fus1.py
@@ -303,10 +303,7 @@ def block_right():
     stop_all() 
 
 def block_left():
-    slide_right(100)
-    time.sleep(0.3)
-    stop_moving()
-    while bk.get_distance() <= 160:
+    while bk.get_distance() <= 155:
         box(100)
         debug.show(bk.get_distance(), wait=False)
         move_forward(300)

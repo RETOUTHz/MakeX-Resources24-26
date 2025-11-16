088fd075cfd0fe2b93a3e27ea80f05956c338a85
Peakkitwarit <peak.kitwarit@gmail.com>
2025-11-16 12:54:24 +0700

    asd

--- 2025FUSION/Program/fus1.py
+++ 2025FUSION/Program/fus1.py
@@ -186,8 +186,8 @@ class controller():
             stop_all()
 
         elif gamepad.is_key_pressed("L2"):
-            shooter_angle(73)
-            shooting(85)
+            shooter_angle(23)
+            shooting(95)
 
         elif gamepad.is_key_pressed("L_Thumb"):
             shooter_angle(70)
@@ -213,7 +213,7 @@ class controller():
             shooter_angle(83)
 
         elif gamepad.is_key_pressed("Down"):
-            shooter_angle(20)
+            shooter_angle(30)
         
         elif gamepad.is_key_pressed("Right"):
             shooter_angle(15)

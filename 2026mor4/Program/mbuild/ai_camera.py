30ce71753e97de14374e738b4d4e36649ef0d71c
RETOUTHz <uoo082551@gmail.com>
2026-04-09 15:20:19 +0700

    asd

---
+++ 2026mor4/Program/mbuild/ai_camera.py
@@ -0,0 +1,57 @@
+class ai_camera_class():
+    def __init__(self, PORT="PORT1", INDEX="INDEX1"):
+        """
+        define the ai camera
+        PORT, the port the sensor is connected to on the novapi
+        INDEX, the sensor number in the chain INDEX<1-10>
+        """
+        pass
+    
+    def ai_camera_face_learn(face:str,index:int):
+        """
+        Learn a face with a specific name and ID.
+        """
+        return
+
+    def ai_camera_face_name_set(face1:str,face2:str,index:int):
+        """
+        Set the name of a learned face.
+        """
+        return
+
+    def ai_camera_face_delete_all(index:int):
+        """
+        Delete all learned faces.
+        """
+        return
+
+    def i_camera_face_delete_by_id(face:str,index:int):
+        """
+        Delete a learned face by ID.
+        """
+        return
+
+    def ai_camera_object_name_set(Item1:str,Item2:str,index:int):
+        """
+        Set the name of a learned object.
+        """
+        return
+
+    def ai_camera_object_name_delete(Item:str,index:int):
+        """
+        Delete a learned object by ID.
+        """
+        return
+    
+    def ai_camera_speech_recognition_set_keyword(keyword:str,index:int):
+        """
+        Set the keyword for speech recognition.
+        """
+        return
+    
+    def ai_camera_image_load_model_by_id(model:int,index:int):
+        """
+        Load a custom model by ID.
+        """
+        return
+
